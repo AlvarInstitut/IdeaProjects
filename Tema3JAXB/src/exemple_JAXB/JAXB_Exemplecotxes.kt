0607b1decb6a63edd7f5a56b3936ed618591dbeb
alvar <alvarser@gmail.com>
2021-09-27 08:57:58 +0200

    T3-2

---
+++ Tema3JAXB/src/exemple_JAXB/JAXB_Exemplecotxes.kt
@@ -0,0 +1,18 @@
+package exemple_JAXB
+
+import java.io.File
+import javax.xml.bind.JAXBContext
+import javax.xml.bind.JAXBException
+
+fun main() {
+    try {
+        val file = File("cotxes.xml")
+        val jaxbContext = JAXBContext.newInstance(Oferta::class.java)
+        val jaxbUnmarshaller = jaxbContext.createUnmarshaller()
+        val oferta = jaxbUnmarshaller.unmarshal(file) as Oferta
+        for (v in oferta.getVehiculo())
+            println(v.getMatricula() + " (" + v.getMarca() + ")")
+    } catch (e: JAXBException) {
+        e.printStackTrace()
+    }
+}
