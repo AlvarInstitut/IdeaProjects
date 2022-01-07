c3ad30a15a835ca70da6e4d372c3fb828b826619
AlvarInstitut <alvar@ieselcaminas.org>
2022-01-07 19:41:42 +0100

    T7 Acabat 2

--- Tema7/src/exercicis/T7Ex4_Mastermind.kt
+++ Tema7/src/exercicis/T7Ex4_Mastermind.kt
@@ -1,13 +1,9 @@
 package exercicis
 
 import com.google.auth.oauth2.GoogleCredentials
-import com.google.cloud.firestore.DocumentChange
-import com.google.cloud.storage.Bucket
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
-import com.google.firebase.cloud.FirestoreClient
 import com.google.firebase.database.*
-import exemples_realtimedatabase.Missatge
 import java.awt.*
 import java.io.FileInputStream
 import javax.swing.*
