f6ce00f41109365df6ba474aa0a74e5205388c37
AlvarInstitut <alvar@ieselcaminas.org>
2022-01-21 09:14:31 +0100

    T8 finalitzat

--- Tema8_2/src/exemples/ProvaServidor1.kt
+++ Tema8_2/src/exemples/ProvaServidor1.kt
@@ -6,16 +6,19 @@ import org.bson.Document
 import java.util.logging.Level
 import java.util.logging.LogManager
 
-
 fun main(){
     LogManager.getLogManager().getLogger("").setLevel(Level.SEVERE)
 
     val con = MongoClient(MongoClientURI("mongodb://ad:ieselcaminas@89.36.214.106/?authSource=test"))
     val bd = con.getDatabase("test")
 
-    val doc = Document()
-    doc.put("msg4","Missatge inserit des de Kotlin")
-    val c = bd.getCollection("exemple").insertOne(doc)
+    val ordenar = Document()
+    ordenar.put("precio", -1)
+
+    val llibres = bd.getCollection("libro").find().sort(ordenar)
+
+    for (llibre in llibres)
+        System.out.println(llibre.get("titulo").toString() + " --> " + llibre.get("precio"))
 
     con.close()
 }
